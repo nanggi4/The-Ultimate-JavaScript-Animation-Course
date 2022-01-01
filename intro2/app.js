6a6ad2ce35e794e59c4e10354f617a79149a5dca
nanggi4 <nkhun1006@gmail.com>
2022-01-01 23:17:31 +0900

    intro commit

--- intro2/app.js
+++ intro2/app.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('button');
 const nav = document.querySelector('nav');
+const p = document.querySelector('p');
 
 button.addEventListener('click', () => {
-  nav.classList.add('nav-slide');
+  nav.classList.toggle('nav-slide');
+  p.classList.toggle('fade');
 });
